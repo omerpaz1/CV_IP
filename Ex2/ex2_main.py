9d78d353b05114c3f29d28916daa4f3f4efffd46
PAZJR93 <omerpaz93@gmail.com>
2020-05-10 10:51:34 +0300

    start canny

--- Ex2/ex2_main.py
+++ Ex2/ex2_main.py
@@ -3,6 +3,15 @@ import matplotlib.pyplot as plt
 import cv2
 from ex2_utils import *
 
+def printByName(listImg):
+    fig = plt.figure(figsize=(20, 10))
+    for i in range(len(listImg)):
+        a = fig.add_subplot(2,3,i+1)
+        plt.imshow(listImg[i],'gray')
+        a.set_title(f'image{[i]}')
+    plt.show()
+
+
 
 def main():
     print("ID:", myID())
@@ -10,20 +19,13 @@ def main():
     Orignal_img = cv2.imread(img_path,0)
     #save the original shape of the image.
     # ---------------------------- fun 1 ----------------------------:
-    # kernel_size = 15
-    # kernel = np.ones((kernel_size,kernel_size))
-    # kernel = kernel/kernel.sum()
-    # print(len(kernel))
-    # myans = conv2D(Orignal_img,kernel)
-    # CvAnsImg = cv2.filter2D(Orignal_img,-1,kernel,borderType=cv2.BORDER_REPLICATE) #need to be 'full'
-    # print(myans)
-    # print(CvAnsImg)
-    # print(np.array_equal(myans, CvAnsImg))
-    # f, ax = plt.subplots(1,2)
-
-    # ax[0].imshow(myans,cmap='gray')
-    # ax[1].imshow(CvAnsImg,cmap='gray')
-    # plt.show()
+    kernel_size = 5
+    kernel = np.ones((kernel_size,kernel_size))
+    print(kernel)
+    kernel = kernel/np.sum(kernel)
+    myans = conv2D(Orignal_img,kernel)
+    CvAnsImg = cv2.filter2D(Orignal_img,-1,kernel) #need to be 'full'
+    diff = np.max(CvAnsImg-myans)
     # ---------------------------- fun 2 ----------------------------:
     # img1DArray = Orignal_img.reshape(-1)
     # imgMatArray = img1DArray.reshape(Orignal_img.shape)
@@ -36,7 +38,12 @@ def main():
     # print('******')
     # print(CvAnsImg)
 
-    convDerivative(Orignal_img)
+    # directions,magnitude,x_der,y_der = convDerivative(Orignal_img)
+    # printByName([Orignal_img,directions,magnitude,x_der,y_der])
 
+    # ---------------------------- fun 3 ----------------------------:
+    myimp, sobleimp = edgeDetectionSobel(Orignal_img)
+    # zero_cross_img = edgeDetectionZeroCrossingSimple(Orignal_img)
+    printByName([myimp,sobleimp])
 if __name__ == '__main__':
     main()
--- Ex2/ex2_utils.py
+++ Ex2/ex2_utils.py
@@ -86,17 +86,103 @@ def  convDerivative(inImage:np.ndarray)  ->  (np.ndarray,np.ndarray,np.ndarray,n
 
     k_x = np.array([1,0,-1])
     k_y = np.reshape(np.array([1,0,-1]),(3,1))
-    x_der = conv2D(inImage,k_x)
-    y_der = conv2D(inImage,k_y)
+    der_X = conv2D(inImage,k_x)
+    der_y = conv2D(inImage,k_y)
 
-    directions = np.arctan2(x_der,y_der)
-    magnitude = np.sqrt(x_der**2 + y_der**2)
-    return (directions,magnitude,x_der,y_der)
+    directions = np.arctan2(der_X,der_y)
+    magnitude = np.sqrt(der_X**2 + der_y**2)
+    return (directions,magnitude,der_X,der_y)
     # f, ax = plt.subplots(1,2)
     # ax[0].imshow(gradient,cmap='gray')
     # ax[1].imshow(mag,cmap='gray')
     # plt.show()
-    # f, ax = plt.subplots(1,2)
-    # ax[0].imshow(x_der,cmap='gray')
-    # ax[1].imshow(y_der,cmap='gray')
-    # plt.show()
+
+def  edgeDetectionSobel(img:  np.ndarray,  thresh:  float  =  0.7)-> (np.ndarray, np.ndarray):
+ 
+    """
+    Detects  edges  using  the  Sobel  method
+    :param  img:  Input  image
+    :param  thresh:  The  minimum  threshold  for  the  edge  response
+    :return:  opencv  solution,  my  implementation
+
+    """
+    h,w = img.shape[:2]
+    ker_Sobol_x = np.array([[1,0,-1],[-2,0,2],[1,0,-1]])
+    ker_Sobol_T_x = ker_Sobol_x.T
+    ker_Sobol_y = np.array([[-1,-2,-1],[0,0,0],[1,2,1]])
+    ker_Sobol_T_y = ker_Sobol_y.T
+    Smoth_x = conv2D(img,ker_Sobol_x)
+    der_x = conv2D(Smoth_x,ker_Sobol_T_x)
+    Smoth_y = conv2D(img, ker_Sobol_y)
+    der_y = conv2D(Smoth_y, ker_Sobol_T_y)
+    magnitude = np.sqrt(der_x**2 + der_y**2)
+    magnitude = cv2.normalize(magnitude,magnitude,0.0,1.0,cv2.NORM_MINMAX, cv2.CV_32FC1)
+    # for i in range(h):
+    #     for j in range(w):
+    #         if magnitude[i,j] >= thresh:
+    #             magnitude[i,j] = 1
+    #         else:
+    #             magnitude[i,j] = 0
+    # sobol algorigtem:
+    sobel_der_x = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)
+    sobel_der_y = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)
+    magnitude_sobol = np.sqrt(sobel_der_x**2+sobel_der_y**2)
+    magnitude_sobol = cv2.normalize(magnitude_sobol,magnitude_sobol,0.0,1.0,cv2.NORM_MINMAX, cv2.CV_32FC1)
+    return (magnitude,magnitude_sobol)
+
+
+
+def  edgeDetectionZeroCrossingSimple(img:np.ndarray)->(np.ndarray):
+    """
+    Detecting  edges  using  the  "ZeroCrossingLOG"  method
+    :param  I:  Input  image
+    :return:  :return:  Edge  matrix """
+    ker_gauss = np.array([1,2,1])
+    ker_lap = np.array([[0,1,0],[1,-4,1],[0,1,0]])
+    h,w = img.shape[:2]
+    LoG = cv2.GaussianBlur(img,(5,5),0)
+    LoG = conv2D(LoG, ker_lap)
+    ZeroCrossingImg = np.zeros(LoG.shape)
+    ans2 = np.zeros(LoG.shape)
+    for y in range(1, h - 1):
+        for x in range(1, w - 1):
+            patch = LoG[y-1:y+2, x-1:x+2]
+            p = LoG[y, x]
+            maxP = patch.max()
+            minP = patch.min()
+            if (p > 0):
+                zeroCross = True if minP < 0 else False
+            else:
+                zeroCross = True if maxP > 0 else False
+            if zeroCross:
+                ZeroCrossingImg[y, x] = 1
+
+    return ZeroCrossingImg
+
+
+
+def  edgeDetectionCanny(img:  np.ndarray,  thrs_1:  float,  thrs_2:  float)-> (np.ndarray, np.ndarray): 
+    """
+    Detecting  edges  usint  "Canny  Edge"  method
+    :param  img:  Input  image
+    :param  thrs_1:  T1
+    :param  thrs_2:  T2
+    :return:  opencv  solution,  my  implementation 
+    """
+    h,w = img.shape[:2]
+    ker_Sobol_x = np.array([[1,0,-1],[-2,0,2],[1,0,-1]])
+    ker_Sobol_T_x = ker_Sobol_x.T
+    ker_Sobol_y = np.array([[-1,-2,-1],[0,0,0],[1,2,1]])
+    ker_Sobol_T_y = ker_Sobol_y.T
+    Smoth_x = conv2D(img,ker_Sobol_x)
+    der_x = conv2D(Smoth_x,ker_Sobol_T_x)
+    Smoth_y = conv2D(img, ker_Sobol_y)
+    der_y = conv2D(Smoth_y, ker_Sobol_T_y)
+    magnitude = np.sqrt(der_x**2 + der_y**2)
+    directions = np.arctan2(der_x,der_y)
+    # for i in range(h):
+    #     for j in range(w):
+    #         if magnitude[i,j] >= thresh:
+    #             magnitude[i,j] = 1
+    #         else:
+    #             magnitude[i,j] = 0
