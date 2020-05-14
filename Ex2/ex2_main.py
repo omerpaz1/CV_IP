b0c88fe159e16a452b850b2c2efa4d0d2dcc0aa5
PAZJR93 <omerpaz93@gmail.com>
2020-05-14 12:57:46 +0300

    canny egde d

--- Ex2/ex2_main.py
+++ Ex2/ex2_main.py
@@ -12,12 +12,25 @@ def printByName(listImg):
     plt.show()
 
 
+def mse(imageA, imageB):
+	# the 'Mean Squared Error' between the two images is the
+	# sum of the squared difference between the two images;
+	# NOTE: the two images must have the same dimension
+	err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
+	err /= float(imageA.shape[0] * imageA.shape[1])
+	
+	# return the MSE, the lower the error, the more "similar"
+	# the two images are
+	return err
 
 def main():
     print("ID:", myID())
     Thresh = 0.5
-    img_path = 'boxman.jpg'
+    img_path = 'ema.jpg'
     Orignal_img = cv2.imread(img_path,0)
+    highThreshold = Orignal_img.max() * 0.12
+    lowThreshold = highThreshold * 0.03
+    singel_Thresh = 0.5
     #save the original shape of the image.
     # ---------------------------- fun 1 ----------------------------:
     # kernel = np.array([1,2,3])
@@ -27,7 +40,7 @@ def main():
     # print(img_id)
     # print(test)
 
-    # kernel_size = 5
+    # kernel_size = 6
     # kernel = np.ones((kernel_size,kernel_size))
     # kernel = kernel/np.sum(kernel)
     # myans = conv2D(Orignal_img,kernel)
@@ -35,6 +48,8 @@ def main():
     # diff = np.max(CvAnsImg-myans)
     # print(diff)
     # printByName([myans,CvAnsImg])
+    # print(mse(myans,CvAnsImg))
+
     # ---------------------------- fun 2 ----------------------------:
     # img1DArray = Orignal_img.reshape(-1)
     # imgMatArray = img1DArray.reshape(Orignal_img.shape)
@@ -49,19 +64,27 @@ def main():
 
     # der_x,der_y,magnitude,directions = convDerivative(Orignal_img)
     # printByName([der_x,der_y,magnitude,directions])
-
+    # ---------------------------- fun 2.2 ----------------------------:
+    # burrimg = blurImage2(Orignal_img,5)
+    # burrGar = cv2.GaussianBlur(Orignal_img,(5,5),0)
+    # diff = np.max(burrGar-burrimg)
+    # print(diff)
     # ---------------------------- fun 3 ----------------------------:
-    MyImpSobol, OpenCvSobol = edgeDetectionSobel(Orignal_img,Thresh)
-    # # zero_cross_img = edgeDetectionZeroCrossingSimple(Orignal_img)
-    # printByName([MyImpSobol, OpenCvSobol])
+    # MyImpSobol,OpenCvSobol = edgeDetectionSobel(Orignal_img,Thresh)
+    # NMS = edgeDetectionCanny(Orignal_img,highThreshold,lowThreshold)
+    # print(mse(MyImpSobol,OpenCvSobol))
+    # zero_cross = edgeDetectionZeroCrossingSimple(Orignal_img)
+    # log = edgeDetectionZeroCrossingLOG(Orignal_img)
+    # print(mse(zero_cross,log))
+    # print()
+    # printByName([MyImpSobol,NMS])
+
+    NMS = edgeDetectionCanny(Orignal_img,highThreshold,lowThreshold)
+    canny = cv2.Canny(Orignal_img,0.2,0.03)
     f, ax = plt.subplots(1, 2)
-    ax[0].imshow(MyImpSobol,'gray')
-    ax[1].imshow(OpenCvSobol,'gray')
+    ax[0].imshow(NMS,'gray')
+    ax[1].imshow(canny,'gray')
     plt.show()
-    # NMS = edgeDetectionCanny(Orignal_img,100,200)
-    # cany = cv2.Canny(Orignal_img,100,200)
-    # plt.imshow(cany,cmap='gray')
-    # plt.show()
     # gray_image = cv2.GaussianBlur(Orignal_img,(5,5),0)
     # cany = HoughLines(Orignal_img,d)
     # printByName([cany[0],cany[1]])
--- Ex2/ex2_utils.py
+++ Ex2/ex2_utils.py
@@ -44,25 +44,13 @@ def conv2D(inImage:np.ndarray,kernel2:np.ndarray)->np.ndarray:
     :param  kernel2:  A  kernel
     :return:  The  convolved  image 
     """
-    if((kernel2.shape[0] % 2) == 0):
-        print('here')
-        pad_size  = int(kernel2.shape[0]/2)
-        print(pad_size)
-    else:
-        pad_size = int((kernel2.shape[0]-1)/2)
-
-    padded_image = np.pad(inImage, ((pad_size,pad_size),(pad_size,pad_size)), 'edge')
-    new_image = np.zeros(padded_image.shape)
-    for i in range(pad_size, padded_image.shape[0]-pad_size):
-        #range(2,802)
-        for j in range(pad_size, padded_image.shape[1]-pad_size):
-            #range(2,1282)
-            sub_image = padded_image[i-pad_size:i+pad_size+1,j-pad_size:j+pad_size+1]
-            prod = sub_image*kernel2
-            sub_image_sum = np.sum(prod)
-            new_image[i,j] = sub_image_sum
-    return new_image[pad_size:-pad_size,pad_size:-pad_size]
-
+    kernel2 = np.flip(kernel2)
+    padded_image = np.pad(inImage, (kernel2.shape[0] // 2, kernel2.shape[1] // 2), 'edge')
+    new_image = np.ndarray(inImage.shape)
+    for i in range(0, new_image.shape[0]):
+        for j in range(0, new_image.shape[1]):
+            new_image[i, j] = np.multiply(padded_image[i:i + kernel2.shape[0], j:j + kernel2.shape[1]], kernel2).sum()
+    return new_image
 
 
 def  convDerivative(inImage:np.ndarray)  ->  (np.ndarray,np.ndarray,np.ndarray,np.ndarray): 
@@ -73,42 +61,64 @@ def  convDerivative(inImage:np.ndarray)  ->  (np.ndarray,np.ndarray,np.ndarray,n
     """
 
     k_x = np.array([1,0,-1])
-    # np.array([1,0,-1])
     k_y = np.reshape(k_x,(3,1))
-    der_X = conv2D(inImage,k_x)
+    der_x = conv2D(inImage,k_x)
     der_y = conv2D(inImage,k_y)
 
-    directions = np.arctan2(der_X,der_y)
-    magnitude = np.sqrt(der_X**2 + der_y**2)
-    return (directions,magnitude,der_X,der_y)
+    directions = np.arctan2(der_y,der_x)
+    magnitude = np.sqrt(der_x**2 + der_y**2)
+    return (directions,magnitude,der_x,der_y)
 
+def blurImage1(in_image: np.ndarray, kernel_size: np.ndarray) -> np.ndarray:
+    """
+    Blur an image using a Gaussian kernel
+    :param inImage: Input image
+    :param kernelSize: Kernel size
+    :return: The Blurred image
+    """
+def  blurImage2(in_image:np.ndarray,kernel_size:np.ndarray)->np.ndarray: 
+    """
+    Blur  an  image  using  a  Gaussian  kernel  using  OpenCV  built-in  functions
+    :param  inImage:  Input  image
+    :param  kernelSize:  Kernel  size
+    :return:  The  Blurred  image 
+    """
+    kernel_x = cv2.getGaussianKernel(kernel_size,0)
+    kernel_y = cv2.getGaussianKernel(kernel_size,0)
+    k = np.dot(kernel_x,kernel_y.T)
+    return cv2.filter2D(in_image,-1,k)
 
 def  edgeDetectionSobel(img:  np.ndarray,  thresh:  float  =  0.7)-> (np.ndarray, np.ndarray):
- 
     """
     Detects  edges  using  the  Sobel  method
     :param  img:  Input  image
     :param  thresh:  The  minimum  threshold  for  the  edge  response
     :return:  opencv  solution,  my  implementation
-
     """
     ker_Sobol_x = np.array([[-1,0,1],[-2,0,2],[-1,0,1]])
     ker_Sobol_y = np.array([[-1,-2,-1],[0,0,0],[1,2,1]])
-    der_x = conv2D(img,ker_Sobol_x)
-    der_y = conv2D(img,ker_Sobol_y)
-    x_n_y = np.add(der_x,der_y)
-    x_n_y = x_n_y.astype(np.float) / 255
-    x_n_y = np.absolute(x_n_y)
-    x_n_y[x_n_y > thresh ] = 1
-    x_n_y[x_n_y < thresh ] = 0
+
+    magnitude = np.sqrt(conv2D(img,ker_Sobol_x)**2 + conv2D(img,ker_Sobol_y)**2)
+    magnitude = magnitude.astype(np.float) / 255
+    magnitude[magnitude > thresh] = 1
+    magnitude[magnitude < thresh] = 0
+
     # sobol algorigtem:
-    ori_sobel_x = cv2.Sobel(img,-1,0,1)
-    ori_sobel_y = cv2.Sobel(img,-1,1,0)
-    sobol_add =np.add(ori_sobel_x,ori_sobel_y) 
-    print(x_n_y)
+    magnitude_sobel = cv2.magnitude(cv2.Sobel(img,cv2.CV_64F,0,1,thresh),cv2.Sobel(img,cv2.CV_64F,1,0,thresh))
+    magnitude_sobel = magnitude_sobel / 255
+    magnitude_sobel[magnitude_sobel > thresh] = 1
+    magnitude_sobel[magnitude_sobel < thresh] = 0
 
-    return (x_n_y,sobol_add)
+    return magnitude,magnitude_sobel
 
+
+def  edgeDetectionZeroCrossingLOG(img:np.ndarray)->(np.ndarray):
+    """
+    Detecting  edges  using  the  "ZeroCrossingLOG"  method
+    :param  I:  Input  image
+    :return:  :return:  Edge  matrix 
+    """
+    return edgeDetectionZeroCrossingSimple(cv2.GaussianBlur(img,(5,5),0))
 
 
 def  edgeDetectionZeroCrossingSimple(img:np.ndarray)->(np.ndarray):
@@ -116,26 +126,13 @@ def  edgeDetectionZeroCrossingSimple(img:np.ndarray)->(np.ndarray):
     Detecting  edges  using  the  "ZeroCrossingLOG"  method
     :param  I:  Input  image
     :return:  :return:  Edge  matrix """
+    imga = cv2.GaussianBlur(img,(5,5),0)
     ker_lap = np.array([[0,1,0],[1,-4,1],[0,1,0]])
-    h,w = img.shape[:2]
-    LoG = conv2D(img, ker_lap)
-    ZeroCrossingImg = np.zeros(LoG.shape)
-    ans2 = np.zeros(LoG.shape)
-
-    for y in range(1, h - 1):
-        for x in range(1, w - 1):
-            patch = LoG[y-1:y+2, x-1:x+2]
-            p = LoG[y, x]
-            maxP = patch.max()
-            minP = patch.min()
-            if (p > 0):
-                zeroCross = True if minP < 0 else False
-            else:
-                zeroCross = True if maxP > 0 else False
-            if zeroCross:
-                ZeroCrossingImg[y, x] = 1
-
-    return ZeroCrossingImg
+    LOG = conv2D(imga, ker_lap)
+    minLoG = cv2.morphologyEx(LOG, cv2.MORPH_ERODE, np.ones((3,3)))
+    maxLoG = cv2.morphologyEx(LOG, cv2.MORPH_DILATE, np.ones((3,3)))
+    zeroCross = np.logical_or(np.logical_and(minLoG < 0,  LOG > 0), np.logical_and(maxLoG > 0, LOG < 0)).astype('int')
+    return zeroCross
 
 
 
@@ -147,24 +144,102 @@ def  edgeDetectionCanny(img:  np.ndarray,  thrs_1:  float,  thrs_2:  float)-> (n
     :param  thrs_2:  T2
     :return:  opencv  solution,  my  implementation 
     """
-    ker_size = 5
-    h,w = img.shape[:2]
-    ker_Sobol_x = np.array([[-1,0,1],[-2,0,2],[-1,0,1]])
-    ker_Sobol_y = np.array([[-1,-2,-1],[0,0,0],[1,2,1]])
+    Kx = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], np.float32)
+    Ky = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]], np.float32)
+    
+    Ix = conv2D(img, Kx)
+    Iy = conv2D(img, Ky)
+    
+    G = np.hypot(Ix, Iy)
+    G = G / G.max() * 255
+    theta = np.arctan2(Iy, Ix)
+    img_non_max = non_max_suppression(G,theta)
+    res, weak, strong = threshold(img_non_max,thrs_1,thrs_2)
+    ans = hysteresis(res, weak, strong)
+    return ans
 
-    der_x = conv2D(img,ker_Sobol_x)
-    der_y = conv2D(img, ker_Sobol_y)
 
-    magnitude = np.sqrt(der_x**2 + der_y**2)
-    directions = np.degrees(np.arctan2(der_x,der_y))
+def non_max_suppression(img, D):
+    M, N = img.shape
+    Z = np.zeros((M,N), dtype=np.int32)
+    angle = D * 180. / np.pi
+    angle[angle < 0] += 180
 
-    for x in range(h):
-        for y in range(w):
-            point = directions[x,y] 
-            if point < 0:
-                directions[x,y] = point+180
-            pivot = directions[x,y]
-            print(pivot)
-            neighbors = directions[y-ker_size:y+ker_size+1,x-ker_size:x+ker_size+1]
-    return directions
+    
+    for i in range(1,M-1):
+        for j in range(1,N-1):
+            try:
+                q = 255
+                r = 255
+                
+               #angle 0
+                if (0 <= angle[i,j] < 22.5) or (157.5 <= angle[i,j] <= 180):
+                    q = img[i, j+1]
+                    r = img[i, j-1]
+                #angle 45
+                elif (22.5 <= angle[i,j] < 67.5):
+                    q = img[i+1, j-1]
+                    r = img[i-1, j+1]
+                #angle 90
+                elif (67.5 <= angle[i,j] < 112.5):
+                    q = img[i+1, j]
+                    r = img[i-1, j]
+                #angle 135
+                elif (112.5 <= angle[i,j] < 157.5):
+                    q = img[i-1, j-1]
+                    r = img[i+1, j+1]
 
+                if (img[i,j] >= q) and (img[i,j] >= r):
+                    Z[i,j] = img[i,j]
+                else:
+                    Z[i,j] = 0
+
+            except IndexError as e:
+                pass
+    
+    return Z
+
+def threshold(img, highThreshold, lowThreshold):
+
+    M, N = img.shape
+    res = np.zeros((M,N), dtype=np.int32)
+    
+    weak = 25
+    strong = 255
+    
+    strong_i, strong_j = np.where(img >= highThreshold)
+    zeros_i, zeros_j = np.where(img < lowThreshold)
+    
+    weak_i, weak_j = np.where((img <= highThreshold) & (img >= lowThreshold))
+    
+    res[strong_i, strong_j] = strong
+    res[weak_i, weak_j] = weak
+    return (res, weak, strong)
+
+
+
+def hysteresis(img, weak, strong):
+    M, N = img.shape  
+    for i in range(1, M-1):
+        for j in range(1, N-1):
+            if (img[i,j] == weak):
+                try:
+                    if ((img[i+1, j-1] == strong) or (img[i+1, j] == strong) or (img[i+1, j+1] == strong)
+                        or (img[i, j-1] == strong) or (img[i, j+1] == strong)
+                        or (img[i-1, j-1] == strong) or (img[i-1, j] == strong) or (img[i-1, j+1] == strong)):
+                        img[i, j] = strong
+                    else:
+                        img[i, j] = 0
+                except IndexError as e:
+                    pass
+    return img
+
+
+def  houghCircle(img:np.ndarray,min_radius:float,max_radius:float)->list:
+    """
+    Find  Circles  in  an  image  using  a  Hough  Transform  algorithm  extension
+    :param  I:  Input  image
+    :param  minRadius:  Minimum  circle  radius
+    :param  maxRadius:  Maximum  circle  radius
+    :return: A list containing the detected circles, [(x,y,radius),(x,y,radius),...]
+    """
